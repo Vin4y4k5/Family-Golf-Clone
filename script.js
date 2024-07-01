ded8e7006eab3ba2c5cb64f8542442a64828cc1a
vinayak4523 <random105264@gmail.com>
2024-07-02 03:59:27 +0530

    page 1 done

---
+++ script.js
@@ -0,0 +1,40 @@
+
+var crsr = document.querySelector("#cursor");
+var crsrblur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function(dets){
+  crsr.style.left = dets.x+ "px"
+  crsr.style.top = dets.y+ "px"
+  crsrblur.style.left = dets.x- 200 + "px" 
+  crsrblur.style.top = dets.y- 200 + "px" 
+
+})
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 1,
+  height: "16vh",
+  scrollTrigger: {
+    trigger:"nav",
+    scroller:"body",
+    markers :true,
+    start:"top -10%",
+    end: "top -11%",
+    scrub: 2
+  }
+})
+
+gsap.to("#main", {
+  backgroundColor: "rgba(0, 0, 0, 0.9)",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    markers: true,
+    start: "top -30%",
+    end: "top -80%",
+    scrub: 1
+
+  }
+})
+
+// 55:40
